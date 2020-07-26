fb09d37b1acd49d9d1b2ad17837875523842a3af
Ty Sarna <ty@sarna.org>
2020-07-26 12:44:20 -0400

    Test ability of Authenticator to add values to the context

--- middleware/basic_auth_test.go
+++ middleware/basic_auth_test.go
@@ -16,7 +16,7 @@ func TestBasicAuth(t *testing.T) {
 	realm := "myRealm"
 
 	acceptedCreds := map[string]string{
-		"gooduser": "goodpassword",
+		"user": "password",
 	}
 
 	r := chi.NewRouter()
@@ -25,10 +25,10 @@ func TestBasicAuth(t *testing.T) {
 		_, _ = w.Write([]byte("Hello World"))
 	})
 
-	testAuth(t, r, "gooduser", "goodpassword", 200, nil)
-	testAuth(t, r, "gooduser", "badpassword", 401, &realm)
-	testAuth(t, r, "baduser", "goodpassword", 401, &realm)
-	testAuth(t, r, "baduser", "badpassword", 401, &realm)
+	testAuth(t, r, "user", "password", 200, "Hello World", nil)
+	testAuth(t, r, "user", "bad", 401, "", &realm)
+	testAuth(t, r, "bad", "password", 401, "", &realm)
+	testAuth(t, r, "bad", "bad", 401, "", &realm)
 }
 
 func TestBasicAuthWithAuthenticator(t *testing.T) {
@@ -37,14 +37,24 @@ func TestBasicAuthWithAuthenticator(t *testing.T) {
 	r := chi.NewRouter()
 	r.Use(BasicAuthWithAuthenticator(realm, TestAuthenticator{t}))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Hello World"))
+		user := getUserName(r.Context())
+		if user != "" {
+			_, _ = w.Write([]byte("Hi " + user))
+		} else {
+			_, _ = w.Write([]byte("Hello World"))
+		}
 	})
 
-	testAuth(t, r, "gooduser", "true", 200, nil)
-	testAuth(t, r, "gooduser", "false", 401, &realm)
+	// Test unaltered context
+	testAuth(t, r, "", "true", 200, "Hello World", nil)
+	testAuth(t, r, "", "false", 401, "", &realm)
+
+	// Test that authenticator is able to add values to the context
+	testAuth(t, r, "Bob", "true", 200, "Hi Bob", nil)
+	testAuth(t, r, "Bob", "false", 401, "", &realm)
 }
 
-func testAuth(t *testing.T, r *chi.Mux, user string, password string, expectedCode int, realm *string) {
+func testAuth(t *testing.T, r *chi.Mux, user string, password string, expectedCode int, expectedBody string, realm *string) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	req.SetBasicAuth(user, password)
 	w := httptest.NewRecorder()
@@ -52,6 +62,13 @@ func testAuth(t *testing.T, r *chi.Mux, user string, password string, expectedCo
 	if w.Code != expectedCode {
 		t.Errorf("With %s:%s expected status code to be %d but got %d", user, password,
 			expectedCode, w.Code)
+	}
+
+	if w.Code == 200 {
+		body := w.Body.String()
+		if body != expectedBody {
+			t.Errorf(" Got '%s' instead of expected body '%s'", body, expectedBody)
+		}
 	}
 
 	if realm != nil {
@@ -63,11 +80,15 @@ func testAuth(t *testing.T, r *chi.Mux, user string, password string, expectedCo
 	}
 }
 
+type ctxKeyUserName int
+
+const UserNameKey ctxKeyUserName = 0
+
 type TestAuthenticator struct {
 	t *testing.T
 }
 
-func (auth TestAuthenticator) CheckPassword(ctx context.Context, _ string, password string) (bool, context.Context) {
+func (auth TestAuthenticator) CheckPassword(ctx context.Context, user string, password string) (bool, context.Context) {
 	if ctx == nil {
 		auth.t.Errorf("Didn't receive incoming context")
 		return false, nil
@@ -78,5 +99,20 @@ func (auth TestAuthenticator) CheckPassword(ctx context.Context, _ string, passw
 		auth.t.Fatalf("Unexpected failure, bad password %s: %s", password, err)
 	}
 
-	return authOk, nil
+	var newCtx context.Context
+	if user != "" {
+		newCtx = context.WithValue(ctx, UserNameKey, user)
+	}
+
+	return authOk, newCtx
+}
+
+func getUserName(ctx context.Context) string {
+	if ctx == nil {
+		return ""
+	}
+	if username, ok := ctx.Value(UserNameKey).(string); ok {
+		return username
+	}
+	return ""
 }
